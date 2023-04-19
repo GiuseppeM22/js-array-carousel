67df45a6c0892a31046c57b0b855dc62ccb97277
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-20 01:12:57 +0200

    add ciclo for

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,30 @@
-let images = ['01.webp','02.webp','03.webp','04.webp','05.webp',];
+let images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
+const boxImage = document.getElementById('boxImage');
+let btnLeft = document.getElementById("leftButton")
+let btnRight = document.getElementById("rightButton")
+let imageCurrent = 0
 
-for (let i = 0; i < images.length; i++) {
-    const imagesEl = images[i];
-}
+
+btnRight.addEventListener ("click", function(){
+    
+    for (let i = 0; i < images.length; i++) {
+        let img = document.createElement("img")
+        img.src = images[i]
+        boxImage.append(img) 
+        img.classList.add ("hidden")
+        
+        if (i == imageCurrent){
+             img.classList.remove("hidden")
+        } else if (i != imageCurrent){
+             img.classList.add("hidden")
+
+        }
+
+        
+    }
+    
+    imageCurrent++;
+    
+
+
+}); 
