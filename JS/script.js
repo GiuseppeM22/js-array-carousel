b30c4215b607199e0c1c56724d502e4767fb7ca0
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-19 16:38:02 +0200

    fix

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,5 @@
-let boxImage = document.getElementById ("boxImage")
-let img = document.createElement("img")
-img = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"]
-boxImage.append(img)
-let btnLeft = document.getElementsByClassName("leftButton")
-let btnRight = document.getElementsByClassName("rightButton")
-let currentImage = 0
+let images = ['01.webp','02.webp','03.webp','04.webp','05.webp',];
 
-btnRight.addEventListener ("click", function(){
-
-    for (let i = 0; i < img.length; i++) {
-        const image = img[i];
-        console.log (img[i])
-        
-    }
-}); 
-
-
-
-
-
-
-
-/*for (let i = 0; i < img.length; i++) {
-    const image = img[i];
-    console.log (img[i])
-    
+for (let i = 0; i < images.length; i++) {
+    const imagesEl = images[i];
 }
-*/
